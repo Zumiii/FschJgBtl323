3e65f2c45ca9cd0cea2a9b60126917302d109cb3
Zumi92 <s4@fallschirmjgbtl-323.de>
2020-04-10 10:29:56 +0200

    Fix

--- Addons/meldeblock/script_component.hpp
+++ Addons/meldeblock/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT equipment
-#define COMPONENT_BEAUTIFIED Equipment
+#define COMPONENT meldeblock
+#define COMPONENT_BEAUTIFIED Meldeblock
 #include "\z\fjb_323\addons\main\script_mod.hpp"
 #include "\z\fjb_323\addons\main\script_macros.hpp"
 #include "\z\acex\addons\intelitems\script_component.hpp"
